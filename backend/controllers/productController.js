130dfea3a8b4eb14f3db3e6a83c233918eb996b8
Andrei Barbosa <96220262+andreikestrel@users.noreply.github.com>
2024-10-15 13:31:20 -0300

    bug: colocando o req do controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,14 +1,14 @@
 const Product = require('../models/productModel');
 
 
-const getAllProducts = (res) => {
+const getAllProducts = (req, res) => {
   const products = Product.getProducts();
   res.json(products);
 };
 
 
 // Lógica do relatório
-const getReport = (res) => {
+const getReport = (req, res) => {
   const products = Product.getProducts();
   const totalProducts = products.length;
   const productsByCategory = {};
